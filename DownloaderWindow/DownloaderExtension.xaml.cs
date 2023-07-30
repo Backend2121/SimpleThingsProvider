67f247ff5e209e349e60ce6195435a0fa18bf8eb
Backend2121 <2121masterhack2121@gmail.com>
2023-07-30 16:22:31 +0200

    Downloader complete, featuring stop, pause and resume buttons, still need to style everything

--- DownloaderWindow/DownloaderExtension.xaml.cs
+++ DownloaderWindow/DownloaderExtension.xaml.cs
@@ -7,10 +7,12 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Reflection;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,16 +25,26 @@ using System.Windows.Shapes;
 
 namespace DownloaderExtension
 {
-    public class WebClientIdentified
+    public class IsPaused
     {
-        public WebClient client { get; }
-        public string name { get; }
-        public bool isCancelled { get; set; }
-        public WebClientIdentified(WebClient c, string n) 
+        public bool pause { get; set; }
+        public IsPaused(bool p)
         {
-            client = c;
-            name = n;
-            isCancelled = false;
+            pause = p;
+        }
+    }
+    public class HttpClientPair
+    {
+        public CancellationTokenSource cancellation { get; set; }
+        public string stopName { get; }
+        public string pauseName { get; }
+        public IsPaused isPaused { get; set; }
+        public HttpClientPair(CancellationTokenSource c, string sn, string pn, IsPaused p) 
+        {
+            cancellation = c;
+            stopName = sn;
+            pauseName = pn;
+            isPaused = p;
         }
     }
     public class DownloaderTemplate
@@ -48,16 +60,19 @@ namespace DownloaderExtension
             ColumnDefinition columnDefinition3 = new ColumnDefinition();
             ColumnDefinition columnDefinition4 = new ColumnDefinition();
             ColumnDefinition columnDefinition5 = new ColumnDefinition();
+            ColumnDefinition columnDefinition6 = new ColumnDefinition();
             columnDefinition1.Width = new GridLength(75, GridUnitType.Star);
             columnDefinition2.Width = new GridLength(100, GridUnitType.Star);
             columnDefinition3.Width = new GridLength(70, GridUnitType.Star);
             columnDefinition4.Width = new GridLength(400, GridUnitType.Star);
-            columnDefinition5.Width = new GridLength(100, GridUnitType.Star);
+            columnDefinition5.Width = new GridLength(67, GridUnitType.Star);
+            columnDefinition6.Width = new GridLength(67, GridUnitType.Star);
             grid.ColumnDefinitions.Add(columnDefinition1);
             grid.ColumnDefinitions.Add(columnDefinition2);
             grid.ColumnDefinitions.Add(columnDefinition3);
             grid.ColumnDefinitions.Add(columnDefinition4);
             grid.ColumnDefinitions.Add(columnDefinition5);
+            grid.ColumnDefinitions.Add(columnDefinition6);
             // The 3 labels
             Label titleLabel = new Label();
             Label ETALabel = new Label();
@@ -72,15 +87,18 @@ namespace DownloaderExtension
             percentageLabel.Content = "%";
             // Progress bar
             ProgressBar progressBar = new ProgressBar();
-            // Stop button
+            // Pause/Resume and Stop buttons
             Button stopButton = new Button();
+            Button pauseButton = new Button();
             stopButton.Content = "S";
+            pauseButton.Content = "P";
             Grid.SetColumn(titleLabel, 0);
             Grid.SetColumn(urlLabel, 0);
             Grid.SetColumn(ETALabel, 1);
             Grid.SetColumn(percentageLabel, 2);
             Grid.SetColumn(progressBar, 3);
-            Grid.SetColumn(stopButton, 4);
+            Grid.SetColumn(pauseButton, 4);
+            Grid.SetColumn(stopButton, 5);
             // Setting the margins
             titleLabel.Margin = new Thickness(0, 0, 10, 0);
             ETALabel.Margin = new Thickness(10, 0, 10, 0);
@@ -95,8 +113,24 @@ namespace DownloaderExtension
             grid.Children.Add(ETALabel);
             grid.Children.Add(percentageLabel);
             grid.Children.Add(progressBar);
+            grid.Children.Add(pauseButton);
             grid.Children.Add(stopButton);
             e.Children.Add(grid);
+        }
+    }
+    public class Progress : IProgress<float>
+    {
+        private ProgressBar progressBar;
+        private Label percentageLabel;
+        public void Report(float value)
+        {
+            progressBar.Value = MathF.Round(value, 1);
+            percentageLabel.Content = MathF.Round(value, 1) + "%";
+        }
+        public Progress(ProgressBar pb, Label pl) 
+        {
+            progressBar = pb;
+            percentageLabel = pl;
         }
     }
     public partial class DownloaderWindow : Window
@@ -105,35 +139,10 @@ namespace DownloaderExtension
         private Regex extensionExpression = new("(\\.)+(.{2,4})(?!.*\\1)");
         private ProgressBar progress;
         private Label percentage;
-        public List<WebClientIdentified> webClients = new List<WebClientIdentified>();
+        public List<HttpClientPair> cancellationTokens = new List<HttpClientPair>();
         public DownloaderWindow()
         {
             InitializeComponent();
-            /*int childCounter = 0;
-            string json = "{\"Downloads\":{";
-            // Read from file how many downloads needs to be instantiated
-            downloadNumber = READ_DOWNLOADS;
-
-            // Serializer
-            // Serialize only if download number is grater than 0
-            if (sp.Children.Count > 0 )
-            {
-                foreach (DockPanel dock in sp.Children)
-                {
-                    foreach (Grid grid in dock.Children)
-                    {
-                        UIElementCollection elements = grid.Children;
-                        Label titlelabel = (Label)elements[0];
-                        Label urllabel = (Label)elements[1];
-                        // Serialize child element
-                        json += "\"" + childCounter.ToString() + "\":{\"url\":\"" + urllabel.Content + "\",\"name\":\"" + titlelabel.Content + "\"},";
-                    }
-                    childCounter++;
-                }
-                json = json.Remove(json.Length - 1);
-                json += "}}";
-                File.WriteAllText(".\\downloads.json", json);
-            }*/
         }
         public void addDownload(string title, string url)
         {
@@ -141,100 +150,86 @@ namespace DownloaderExtension
             sp.Children.Add(new DownloaderTemplate(downloadNumber, title, url).e);
             startDownload();
         }
-        public void startDownload()
+        public async void startDownload()
         {
             string url = string.Empty;
             string name = string.Empty;
-            // Read from existing downloads informations needed to start them
+            DockPanel lastChild = null;
+            // Get the last children
             foreach (DockPanel dock in sp.Children)
             {
-                foreach (Grid grid in dock.Children)
+                lastChild = dock;
+            }
+            foreach (Grid grid in lastChild.Children)
+            {
+                // Get UIElements partecipating in the download
+                UIElementCollection col = grid.Children;
+                name = ((Label)col[0]).Content.ToString();
+                url = ((Label)col[1]).Content.ToString();
+                // Pause/Resume button: name and function
+                Button pauseBtn = (Button)col[5];
+                pauseBtn.Name = "PauseButton_" + downloadNumber.ToString();
+                pauseBtn.Click += PauseButton_Click;
+                // Stop button: name and function
+                Button stopBtn = (Button)col[6];
+                stopBtn.Name = "DownloadButton_" + downloadNumber.ToString();
+                stopBtn.Click += StopButton_Click;
+
+                progress = (ProgressBar)col[4];
+                percentage = (Label)col[3];
+                // Find a match in the regex expression
+                Match match = extensionExpression.Match(url);
+                // If match is found
+                if (match.Success)
                 {
-                    UIElementCollection col = grid.Children;
-                    name = ((Label)col[0]).Content.ToString();
-                    url = ((Label)col[1]).Content.ToString();
-                    Button stopBtn = (Button)col[5];
-                    stopBtn.Name = "DownloadButton" + downloadNumber.ToString();
-                    stopBtn.Click += StopButton_Click;
-                    progress = ((ProgressBar)col[4]);
-                    percentage = ((Label)col[3]);
-                    Match match = extensionExpression.Match(url);
-                    if (match.Success)
-                    {
-                        WebClient client = new WebClient();
-                        WebClientIdentified webClientIdentified = new WebClientIdentified(client, downloadNumber.ToString());
-                        //client.Headers WE NEED TO SET THEM
-                        client.DownloadProgressChanged += client_DownloadProgressChanged;
-                        client.DownloadFileCompleted += client_DownloadFileCompleted;
-                        webClients.Add(webClientIdentified);
-                        object[] payload = new object[] { progress, percentage, webClientIdentified };
-                        client.DownloadFileAsync(
-                            // Param1 = Link of file
-                            new Uri(url),
-                            // Param2 = Path to save, need to have this in Settings
-                            "C:\\Users\\alexi\\Desktop\\" + name + match.Value,
-                            payload
-                        );
-                    }
-                    else
-                    {
-                        //Regex match not found, notify user
-                    }
+                    bool isPaused = false;
+                    Stream file = File.Open("C:\\Users\\alexi\\Desktop\\" + FilePathUtils.GetValidFilePath(name) + match.Value, FileMode.OpenOrCreate);
+                    Stream response = await HttpClientSingleton.client.GetStreamAsync(url);
+                    CancellationTokenSource cancellationToken = new CancellationTokenSource();
+                    IsPaused p = new IsPaused(false);
+                    cancellationTokens.Add(new HttpClientPair(cancellationToken, "DownloadButton_" + downloadNumber.ToString(), "PauseButton_" + downloadNumber.ToString(), p));
+                    await HttpClientSingleton.DownloadAsync(HttpClientSingleton.client, url, file, new Progress(progress, percentage), cancellationToken.Token, p);
                 }
             }
         }
+
+        private void PauseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         private void Window_Closing(object sender, CancelEventArgs e)
         {
             e.Cancel = true;
-            this.Visibility = Visibility.Hidden;
-        }
-        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
-        {
-            // Picks the last progress and percentage label in the grid, how to fix this?
-            object[] payload = (object[])e.UserState;
-            ProgressBar pb = (ProgressBar)payload[0];
-            Label p = (Label)payload[1];
-            WebClientIdentified wc = (WebClientIdentified)payload[2];
-
-            if (wc.isCancelled)
-            {
-                wc.client.DownloadProgressChanged -= client_DownloadProgressChanged;
-                wc.client.DownloadFileCompleted -= client_DownloadFileCompleted;
-                wc.client.Dispose();
-                pb.Value = 0;
-                p.Content = "Cancelled";
-                return;
-            }
-            pb.Value = e.ProgressPercentage;
-            p.Content = e.ProgressPercentage + "%";
-        }
-        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
-        {
-            if (e.Cancelled)
-            {
-                // Remove partial file here somehow
-                return;
-            }
-            if (e.Error != null)
-            {
-                throw e.Error;
-            }
-            // Picks the last progress and percentage label in the grid, how to fix this?
-            object[] payload = (object[])e.UserState;
-            ProgressBar pb = (ProgressBar)payload[0];
-            Label p = (Label)payload[1];
-            pb.Value = 100;
-            p.Content = "100%";
+            Visibility = Visibility.Hidden;
         }
         public void StopButton_Click(object sender, RoutedEventArgs e)
         {
-            Button me = (Button)sender;            
-            foreach(WebClientIdentified webClientIdentified in webClients)
+            foreach(HttpClientPair pair in cancellationTokens)
             {
-                if (me.Name.Contains(webClientIdentified.name))
+
+                if (pair.stopName.Equals(((Button)sender).Name))
                 {
-                    Debug.WriteLine(webClientIdentified.client.BaseAddress);
-                    webClientIdentified.isCancelled = true;
+                    pair.cancellation.Cancel();
+                    cancellationTokens.Remove(pair);
+                    break;
+                }
+            }
+        }
+        public void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (HttpClientPair pair in cancellationTokens)
+            {
+                if (pair.pauseName.Equals(((Button)sender).Name))
+                {
+                    if (pair.isPaused.pause)
+                    {
+                        pair.isPaused.pause = false;
+                    }
+                    else
+                    {
+                        pair.isPaused.pause = true;
+                    }
                 }
             }
         }
