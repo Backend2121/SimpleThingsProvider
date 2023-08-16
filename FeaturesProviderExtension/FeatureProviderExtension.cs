ff8b6e05c0a0b77a07a5afc863856e6ebb6ba430
Backend2121 <2121masterhack2121@gmail.com>
2023-08-16 17:18:08 +0200

    Started the feature downloader

---
+++ FeaturesProviderExtension/FeatureProviderExtension.cs
@@ -0,0 +1,68 @@
+﻿using FeaturesProviderExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SimpleThingsProvider
+{
+    partial class FeatureProviderExtension : IExtension
+    {
+        public string name { get { return "Feature Provider"; } set { } }
+        public Window extensionWindow { get; set; }
+        private Window dw;
+        public FeatureProviderExtension()
+        {
+            extensionWindow = new FeatureProviderWindow();
+            dw = (FeatureProviderWindow)extensionWindow;
+        }
+
+        public void disableButton(Label outputLabel)
+        {
+            return;
+        }
+
+        public void enableButton(Label outputLabel)
+        {
+            return;
+        }
+
+        public List<UIElement> getElements(ListView lv, Label ol)
+        {
+            List<UIElement> list = new List<UIElement>();
+            return list;
+        }
+
+        public Window getExtensionWindow()
+        {
+            if (dw == null)
+            {
+                //dw = new FeatureProviderExtension();
+            }
+            return dw;
+        }
+
+        public List<DockPanel> getSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void saveSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void showWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool startFunction(object[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
