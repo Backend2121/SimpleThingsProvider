dfdacd3e8a5b2f44a319e11c0080c357eef4a27c
Backend2121 <2121masterhack2121@gmail.com>
2023-11-11 13:26:10 +0100

    Updated version

--- TorrentScraper/Settings.Designer.cs
+++ TorrentScraper/Settings.Designer.cs
@@ -12,8 +12,8 @@ namespace SimpleThingsProvider {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
+    public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
@@ -85,7 +85,7 @@ namespace SimpleThingsProvider {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2.0.0.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2.0.1.0")]
         public string ApplicationVersion {
             get {
                 return ((string)(this["ApplicationVersion"]));
